9f9edf7dc6f8cf8769bc52a427d551bbbb77d280
fuckupoff <anvesaru@gmail.com>
2025-05-18 16:37:03 +0700

    1.3

---
+++ practicalwork3.py
@@ -0,0 +1,8 @@
+def contains(nums):
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums [i] == nums[j]:
+                return True
+    return False
+print(contains([1,2,3,4]))
+print(contains([1,1,1,3,3,4,3,2,4,2]))
